de3044acee2b0e5d1cc445f490a9973b86547330
William Li <email@williamli.dev>
2021-09-21 12:59:23 +0800

    added official i18next simple example

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
-import '../styles/globals.css'
+import { appWithTranslation } from 'next-i18next'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />
 
-export default MyApp
+export default appWithTranslation(MyApp)
